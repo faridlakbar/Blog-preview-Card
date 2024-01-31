cb3a34d5e8d07b1fc4feccb4fd3b960f8215d1d8
faridl akbarullah <akbar12ullah@gmail.com>
2024-01-31 11:01:14 +0700

    react 1

--- blog-preview-card/src/Card.jsx
+++ blog-preview-card/src/Card.jsx
@@ -5,6 +5,14 @@ import styles from "./CardImage.module.css";
 export default function Card() {
   const stylesSection = {
     display: "flex",
+    alignItems: "center",
+  };
+  const h1 = {
+    fontWeight: 800,
+  };
+  const h2 = {
+    fontWeight: 600,
+    fontSize: "16px",
   };
 
   return (
@@ -14,16 +22,20 @@ export default function Card() {
         className={styles.cardImage}
         alt="image article"
       />
-      <span>Learning</span>
+      <span className={styles.category}>Learning</span>
       <p>Published 21 Dec 2023</p>
-      <h1>HTML & CSS foundations</h1>
-      <p>
+      <h1 style={h1}>HTML & CSS foundations</h1>
+      <p className={styles.p}>
         These languages are the backbone of every website, defining structure,
         content and presentation.
       </p>
       <section style={stylesSection}>
-        <img src={imageProfile} alt="profile image" />
-        <h2>Greg hooper</h2>
+        <img
+          src={imageProfile}
+          className={styles.imageProfile}
+          alt="profile image"
+        />
+        <h2 style={h2}>Greg hooper</h2>
       </section>
     </article>
   );
